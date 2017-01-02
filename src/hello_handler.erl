dbb03d65bef5f1da380e387786b1d58276b8a25a
quantumpotato <quantumpotato@gmail.com>
2017-01-01 22:10:14 -0500

    Show auth after public arena

--- src/hello_handler.erl
+++ src/hello_handler.erl
@@ -4,9 +4,8 @@
 init(Req0, Opts) ->
         Gate = whereis(gate),
         #{name := Name} = cowboy_req:match_qs([{name, [], undefined}], Req0),
-        NameAtom = list_to_atom(binary_to_list(Name)),
-        {JoinedGate, Auth} = gate:action(Gate, {join, NameAtom}),
-        JoinedGate2 = lists:append(JoinedGate, {auth, Auth}),
+        {JoinedGate, Auth} = gate:action(Gate, {join, Name}),
+        JoinedGate2 = lists:append(JoinedGate, [{auth, Auth}]),
         R= io_lib:format("~p",[JoinedGate2]),
         Req = cowboy_req:reply(200, #{
                 <<"content-type">> => <<"text/plain">>
